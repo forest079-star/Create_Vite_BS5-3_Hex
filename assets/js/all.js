96d96ff9b94c4c56b60c53881f311e152b0e2170
forest079 <bluegx08@gmail.com>
2023-08-23 09:32:50 +0800

    初始更新

---
+++ assets/js/all.js
@@ -0,0 +1,13 @@
+$(function () {
+
+  // console.log($);
+
+  // ClassicEditor
+  // ClassicEditor.create(document.querySelector('#editor'))
+  //   .then((editor) => {
+  //     console.log(editor);
+  //   })
+  //   .catch((error) => {
+  //     console.error(error);
+  //   });
+});
---
+++ main.js
@@ -0,0 +1,10 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+import $ from 'jquery';
+window.jQuery = window.$ = $;
+
+
+import './assets/js/all.js';
+
+console.log("main.js 入口檔案");
